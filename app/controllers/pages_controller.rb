2c7ada3843c1683c767e43f8d8d5718662f6a915
Meagan Butters <meagan.butters@hotmail.com>
2020-09-09 19:09:55 -0300

    tyguhj

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,9 +4,15 @@ class PagesController < ApplicationController
     require 'open-uri'
 
     @url = "https://www.thecocktaildb.com/api/json/v1/1/search.php?f=a"
-    cocktail_serialized = open(@url).read
-    cocktail_parsed = JSON.parse(cocktail_serialized)
-    @cocktails = cocktail_parsed["drinks"]
+    letter_serialized = open(@url).read
+    letter_parsed = JSON.parse(letter_serialized)
+    @letter_cocktails = letter_parsed["drinks"]
+
+    @categories = ["Ordinary Drink","Cocktail","Milk / Float / Shake","Other/Unknown","Cocoa","Shot","Coffee / Tea","Homemade Liqueur","Punch / Party Drink","Beer","Soft Drink / Soda"]
+    # @category_url = "https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=#{params[:category]}"
+    # category_serialized = open(@category_url).read
+    # category_parsed = JSON.parse(category_serialized)
+    # @category_cocktails = category_parsed["drinks"]
   end
 
   def index 
@@ -14,9 +20,15 @@ class PagesController < ApplicationController
     require 'open-uri'
     
     @url = "https://www.thecocktaildb.com/api/json/v1/1/search.php?f=#{params[:letter]}"
-    cocktail_serialized = open(@url).read
-    cocktail_parsed = JSON.parse(cocktail_serialized)
-    @cocktails = cocktail_parsed["drinks"]
+    letter_serialized = open(@url).read
+    letter_parsed = JSON.parse(letter_serialized)
+    @letter_cocktails = letter_parsed["drinks"]
+
+    @categories = ["Ordinary Drink","Cocktail","Milk / Float / Shake","Other/Unknown","Cocoa","Shot","Coffee / Tea","Homemade Liqueur","Punch / Party Drink","Beer","Soft Drink / Soda"]
+    # @category_url = "https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=#{params[:category]}"
+    # category_serialized = open(@category_url).read
+    # category_parsed = JSON.parse(category_serialized)
+    # @category_cocktails = category_parsed["drinks"]
   end
 
   def show
@@ -24,5 +36,11 @@ class PagesController < ApplicationController
     cocktail_serialized = open(@url).read
     cocktail_parsed = JSON.parse(cocktail_serialized)
     @cocktail = cocktail_parsed["drinks"]
+  end
+
+  private 
+
+  def cocktail_params
+    params.require(:pages).permit(:category)
   end
 end
